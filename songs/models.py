dac32b7065dc8dc04ab226d67499a98cfead2900
Steve Geluso <stevegeluso@gmail.com>
2017-02-09 13:25:36 -0800

    creating artist, album, song models, creating migration

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+  name = models.CharField(max_length=200)
+
+class Album(models.Model):
+  title = models.CharField(max_length=200)
+  artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+class Song(models.Model):
+  title = models.CharField(max_length=200)
+  album = models.ForeignKey(Album, on_delete=models.CASCADE)
+  artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
